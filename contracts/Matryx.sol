5443d4a731eef93ba20225810f998d02d4902d38
nginnever <ginneversource@gmail.com>
2017-08-26 17:07:13 -0700

    basic compilation

--- contracts/Matryx.sol
+++ contracts/Matryx.sol
@@ -1,7 +1,14 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.13;
 
+import './MatryxBounty.sol';
 
-
+/**
+ * @title Matryx 
+ * @dev Alpha version of the Matryx bounty system. This is a controller
+ * contract to register the bounties and create new bounty contracts
+ *
+ * This contract is meant to provided as a proof of concept only
+ */
 contract Matryx
 {
 
@@ -13,9 +20,10 @@ contract Matryx
         owner = msg.sender;
     }
 
-    function bounty(uint256 start, uint256 end, uint32 rounds, uint256 reviewDelay)
+    // Creates new bounty contracts and registers them
+    function createBounty(uint256 start, uint256 end, uint32 rounds, uint256 reviewDelay)
     {
-        bounties.push(new Bounty(start, end, rounds, reviewDelay));
+        bounties.push(new MatryxBounty(start, end, rounds, reviewDelay));
     }
 
 }
