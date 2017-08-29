c832f9308d04d417488536137cce6c55cfacd335
nginnever <ginneversource@gmail.com>
2017-08-29 09:52:06 -0700

    enforce bounty state

--- contracts/MatryxBounty.sol
+++ contracts/MatryxBounty.sol
@@ -20,6 +20,7 @@ contract MatryxBounty is Ownable
     uint256 public entryFee;
     uint256 public bounty;
     uint256 public roundBounty;
+    uint256 public currentRound;
 
     uint256 public roundOpenDuration;
     uint256 public roundTotalDuration;
@@ -29,7 +30,7 @@ contract MatryxBounty is Ownable
     * - Preparing: before all submissions
     * - Submitting: period in which submissions may be entered
     * - Reviewing: period of time for reward review
-    * - Finalized: After the bounty has completed
+    * - Finalized: After the round has completed
     */
     
     enum State{Preparing, Submitting, Reviewing, Finalized}
@@ -65,6 +66,7 @@ contract MatryxBounty is Ownable
         reviewDelay = _reviewDelay;
         entryFee = _entryFee;
         bounty = 0;
+        currentRound = 0;
         // Change ownership from MatryxPlatform contract to the bounty creator
         owner = tx.origin;
 
@@ -97,13 +99,13 @@ contract MatryxBounty is Ownable
         bounty = bounty.add(msg.value);
     }
 
-    function submit(bytes _submission, address _payout, uint256 _roundNum) canSubmit(_roundNum) payable {
+    function submit(bytes _submission, address _payout) canSubmit(currentRound) payable {
         require(msg.value == entryFee);
         MatryxRound thisRound = MatryxRound(rounds[_roundNum]);
         thisRound.submit.value(msg.value)(_submission, _payout);
     }
 
-    function pay(address _submitter, uint256 _roundNum) onlyOwner canPay(_roundNum) {
+    function pay(address _submitter) onlyOwner canPay(currentRound) {
         MatryxRound r = MatryxRound(rounds[_roundNum]);
         uint256 rating = r.getRating(_submitter);
         uint256 totalRating = r.getTotalRating();
@@ -112,18 +114,19 @@ contract MatryxBounty is Ownable
         r.pay.value(compensation)(_submitter);
     }
 
-    function refund(uint256 _roundNum) canRefund(_roundNum) {
+    function refund() canRefund(currentRound) {
         MatryxRound r = MatryxRound(rounds[_roundNum]);
         r.refund();
     }
 
-    function rate(uint256 _roundNum, address _submitter, uint256 rating) onlyOwner canPay(_roundNum) {
+    function rate(address _submitter, uint256 rating) onlyOwner canPay(currentRound) {
         MatryxRound r = MatryxRound(rounds[_roundNum]);
         r.rate(_submitter, rating);
     }
 
-    function closeRound(uint256 _roundNum, address _winner, uint256 _totalRating) onlyOwner canPay(_roundNum) {
+    function closeRound(address _winner, uint256 _totalRating) onlyOwner canPay(currentRound) {
         MatryxRound r = MatryxRound(rounds[_roundNum]);
+        currentRound++;
         r.close(_winner, _totalRating);
     }
     /**
@@ -137,7 +140,7 @@ contract MatryxBounty is Ownable
         uint256 roundEnd = MatryxRound(rounds[_roundNum]).getEnd();
         uint256 refundStart = MatryxRound(rounds[_roundNum]).getRefundTime();
 
-        if(now < roundStart) {
+        if(now < start) {
             return State.Preparing;
         } else if(now < roundEnd && now > roundStart) {
             return State.Submitting;
